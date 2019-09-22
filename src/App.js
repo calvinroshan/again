382191a0bda090a127618c6c5e18c0b6a67f4c9b
Your Name <you@example.com>
2019-09-22 16:23:05 +0530

    added project

--- src/App.js
+++ src/App.js
@@ -18,8 +18,9 @@ class App extends React.Component{
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
+    console.log(data);
     if(city && country){
       console.log(data);
       this.setState({
